ae4ef87f54ee9d6c69581fb15915f3ab3e5b55af
arsframework <arsframework@hotmail.com>
2017-12-15 14:48:15 +0800

    Add txt2swf method and file2pdf method

--- src/main/java/ars/file/office/Converts.java
+++ src/main/java/ars/file/office/Converts.java
@@ -7,7 +7,15 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.nio.charset.Charset;
 
+import com.artofsolving.jodconverter.DocumentConverter;
+import com.artofsolving.jodconverter.openoffice.connection.OpenOfficeConnection;
+import com.artofsolving.jodconverter.openoffice.connection.SocketOpenOfficeConnection;
+import com.artofsolving.jodconverter.openoffice.converter.OpenOfficeDocumentConverter;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FileUtils;
 import org.apache.fop.svg.PDFTranscoder;
 import org.apache.batik.transcoder.Transcoder;
 import org.apache.batik.transcoder.TranscoderInput;
@@ -16,6 +24,9 @@ import org.apache.batik.transcoder.TranscoderException;
 import org.apache.batik.transcoder.image.PNGTranscoder;
 import org.apache.batik.transcoder.image.JPEGTranscoder;
 
+import ars.util.Files;
+import ars.util.Strings;
+
 /**
  * 文件转换工具类
  * 
@@ -23,6 +34,25 @@ import org.apache.batik.transcoder.image.JPEGTranscoder;
  * 
  */
 public final class Converts {
+	private static String openOfficeHost = Strings.DEFAULT_LOCALHOST_ADDRESS;
+	private static int openOfficePort = SocketOpenOfficeConnection.DEFAULT_PORT;
+
+	public static String getOpenOfficeHost() {
+		return openOfficeHost;
+	}
+
+	public static void setOpenOfficeHost(String openOfficeHost) {
+		Converts.openOfficeHost = openOfficeHost;
+	}
+
+	public static int getOpenOfficePort() {
+		return openOfficePort;
+	}
+
+	public static void setOpenOfficePort(int openOfficePort) {
+		Converts.openOfficePort = openOfficePort;
+	}
+
 	/**
 	 * 将文件转换成swf文件
 	 * 
@@ -36,11 +66,74 @@ public final class Converts {
 	 *             IO操作异常
 	 */
 	private static void file2swf(File input, File output, String handle) throws IOException {
+		if (input == null) {
+			throw new IllegalArgumentException("Illegal input:" + input);
+		}
+		if (output == null) {
+			throw new IllegalArgumentException("Illegal output:" + output);
+		}
+		if (handle == null) {
+			throw new IllegalArgumentException("Illegal handle:" + handle);
+		}
 		String command = new StringBuilder(handle).append(" ").append(input.getPath()).append(" -o ")
 				.append(output.getPath()).append(" -T 9").toString();
 		try {
 			Runtime.getRuntime().exec(command).waitFor();
 		} catch (InterruptedException e) {
+		}
+	}
+
+	/**
+	 * 将文件转换成pdf文件
+	 * 
+	 * @param file
+	 *            源文件
+	 * @return 目标文件
+	 * @throws IOException
+	 *             IO操作异常
+	 */
+	public static File file2pdf(File file) throws IOException {
+		if (file == null) {
+			throw new IllegalArgumentException("Illegal file:" + file);
+		}
+		if (file.getName().toLowerCase().endsWith(".pdf")) {
+			return file;
+		}
+		File output = new File(file.getPath() + ".temp.pdf");
+		file2pdf(file, output);
+		return output;
+	}
+
+	/**
+	 * 将文件转换成pdf文件
+	 * 
+	 * @param input
+	 *            源文件
+	 * @param output
+	 *            目标文件
+	 * @throws IOException
+	 *             IO操作异常
+	 */
+	public static void file2pdf(File input, File output) throws IOException {
+		if (input == null) {
+			throw new IllegalArgumentException("Illegal input:" + input);
+		}
+		if (output == null) {
+			throw new IllegalArgumentException("Illegal output:" + output);
+		}
+		if (!output.exists()) {
+			synchronized (output.getPath().intern()) {
+				if (!output.exists()) {
+					OpenOfficeConnection connection = new SocketOpenOfficeConnection(openOfficeHost, openOfficePort);
+					connection.connect();
+					try {
+						DocumentConverter converter = new OpenOfficeDocumentConverter(connection);
+						converter.convert(input, output);
+					} finally {
+						connection.disconnect();
+					}
+				}
+			}
 		}
 	}
 
@@ -129,6 +222,44 @@ public final class Converts {
 	}
 
 	/**
+	 * 将txt文件转换成swf文件
+	 * 
+	 * @param input
+	 *            输入文件
+	 * @param output
+	 *            输出文件
+	 * @throws IOException
+	 *             IO操作异常
+	 */
+	public static void txt2swf(File input, File output) throws IOException {
+		String encoding = Files.getEncoding(input);
+		String charset = Charset.defaultCharset().name();
+		if (charset.equalsIgnoreCase(encoding)) {
+			File pdf = file2pdf(input);
+			try {
+				file2swf(pdf, output);
+			} finally {
+				pdf.delete();
+			}
+		} else {
+			File temp = new File(input.getPath() + ".temp.txt");
+			OutputStream os = new FileOutputStream(temp);
+			try {
+				IOUtils.write(FileUtils.readFileToString(input, encoding), os, charset);
+			} finally {
+				os.close();
+			}
+			File pdf = file2pdf(temp);
+			try {
+				file2swf(pdf, output);
+			} finally {
+				pdf.delete();
+				temp.delete();
+			}
+		}
+	}
+
+	/**
 	 * 将文件转换成SWF文件
 	 * 
 	 * @param input
@@ -152,8 +283,15 @@ public final class Converts {
 			wav2swf(input, output);
 		} else if (name.endsWith(".ttf")) {
 			font2swf(input, output);
+		} else if (name.endsWith(".txt")) {
+			txt2swf(input, output);
 		} else {
-			throw new IllegalArgumentException("Not support file:" + input);
+			File pdf = file2pdf(input);
+			try {
+				file2swf(pdf, output);
+			} finally {
+				pdf.delete();
+			}
 		}
 	}
 

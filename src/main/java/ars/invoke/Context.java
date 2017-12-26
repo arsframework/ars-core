010c211ed67708ed2d3a13d6c0244e2f27a981c2
arsframework <arsframework@hotmail.com>
2017-12-26 12:04:22 +0800

    Remove getCache method

--- src/main/java/ars/invoke/Context.java
+++ src/main/java/ars/invoke/Context.java
@@ -2,7 +2,6 @@ package ars.invoke;
 
 import ars.invoke.Router;
 import ars.invoke.Messager;
-import ars.invoke.cache.InvokeCache;
 import ars.invoke.request.SessionFactory;
 
 /**
@@ -25,13 +24,6 @@ public interface Context {
 	 * @return 消息国际化处理对象
 	 */
 	public Messager getMessager();
-
-	/**
-	 * 获取系统缓存处理对象
-	 * 
-	 * @return 缓存处理对象
-	 */
-	public InvokeCache getCache();
 
 	/**
 	 * 获取会话工厂对象

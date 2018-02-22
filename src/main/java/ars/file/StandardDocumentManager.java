f3b00a573c2114bc9de9e012f4d6eb0d56696c55
wuyq <314691309@qq.com>
2018-02-22 16:08:54 +0800

    Initial the value of operator to null

--- src/main/java/ars/file/StandardDocumentManager.java
+++ src/main/java/ars/file/StandardDocumentManager.java
@@ -11,7 +11,6 @@ import ars.file.Operator;
 import ars.file.NameGenerator;
 import ars.file.DocumentManager;
 import ars.file.DirectoryGenerator;
-import ars.file.disk.DiskOperator;
 import ars.invoke.request.Requester;
 import ars.invoke.request.ParameterInvalidException;
 
@@ -22,7 +21,7 @@ import ars.invoke.request.ParameterInvalidException;
  * 
  */
 public class StandardDocumentManager implements DocumentManager {
-	private Operator operator = new DiskOperator();
+	private Operator operator; // 文件处理器
 	private NameGenerator nameGenerator; // 文件名称生成器
 	private DirectoryGenerator directoryGenerator; // 文件目录生成器
 
@@ -50,9 +49,19 @@ public class StandardDocumentManager implements DocumentManager {
 		this.directoryGenerator = directoryGenerator;
 	}
 
+	/**
+	 * 文件操作器有效性检查
+	 */
+	protected void checkOperatorValidity() {
+		if (this.operator == null) {
+			throw new RuntimeException("File operator has not been initialize");
+		}
+	}
+
 	@Override
 	public String upload(Requester requester, String path, Nfile file, Map<String, Object> parameters)
 			throws Exception {
+		this.checkOperatorValidity();
 		String name = file.getName();
 		if (this.directoryGenerator != null) {
 			if (path == null) {
@@ -71,28 +80,33 @@ public class StandardDocumentManager implements DocumentManager {
 
 	@Override
 	public Nfile download(Requester requester, String path, Map<String, Object> parameters) throws Exception {
+		this.checkOperatorValidity();
 		return this.operator.read(path);
 	}
 
 	@Override
 	public List<Describe> trees(Requester requester, String path, Map<String, Object> parameters) throws Exception {
+		this.checkOperatorValidity();
 		return this.operator.trees(path, parameters);
 	}
 
 	@Override
 	public String content(Requester requester, String path, Map<String, Object> parameters) throws Exception {
+		this.checkOperatorValidity();
 		return new String(this.operator.read(path).getBytes());
 	}
 
 	@Override
 	public List<Describe> files(Requester requester, String path, boolean spread, Map<String, Object> parameters)
 			throws Exception {
+		this.checkOperatorValidity();
 		return this.operator.query().spread(spread).path(path).custom(parameters).list();
 	}
 
 	@Override
 	public void copy(Requester requester, String[] sources, String target, Map<String, Object> parameters)
 			throws Exception {
+		this.checkOperatorValidity();
 		for (String source : sources) {
 			this.operator.copy(source, target);
 		}
@@ -101,6 +115,7 @@ public class StandardDocumentManager implements DocumentManager {
 	@Override
 	public void move(Requester requester, String[] sources, String target, Map<String, Object> parameters)
 			throws Exception {
+		this.checkOperatorValidity();
 		for (String source : sources) {
 			this.operator.move(source, target);
 		}
@@ -108,6 +123,7 @@ public class StandardDocumentManager implements DocumentManager {
 
 	@Override
 	public void remove(Requester requester, String[] paths, Map<String, Object> parameters) throws Exception {
+		this.checkOperatorValidity();
 		for (String path : paths) {
 			this.operator.delete(path);
 		}
@@ -116,6 +132,7 @@ public class StandardDocumentManager implements DocumentManager {
 	@Override
 	public void append(Requester requester, String path, String content, boolean directory,
 			Map<String, Object> parameters) throws Exception {
+		this.checkOperatorValidity();
 		if (this.operator.exists(path)) {
 			throw new ParameterInvalidException("name", "exist");
 		}
@@ -130,6 +147,7 @@ public class StandardDocumentManager implements DocumentManager {
 
 	@Override
 	public void rename(Requester requester, String path, String name, Map<String, Object> parameters) throws Exception {
+		this.checkOperatorValidity();
 		if (!Files.getName(path).equalsIgnoreCase(name)) {
 			String target = new File(new File(path).getParent(), name).getPath();
 			if (this.operator.exists(target)) {
@@ -142,6 +160,7 @@ public class StandardDocumentManager implements DocumentManager {
 	@Override
 	public void update(Requester requester, String path, String content, Map<String, Object> parameters)
 			throws Exception {
+		this.checkOperatorValidity();
 		if (content == null) {
 			this.operator.write(Streams.EMPTY_ARRAY, path);
 		} else {

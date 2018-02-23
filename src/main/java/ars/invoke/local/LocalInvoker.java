cf5987d28efd376af972eb6d2d3820fbf7549dc3
wuyq <314691309@qq.com>
2018-02-23 17:11:36 +0800

    Remove the method of getParameters

--- src/main/java/ars/invoke/local/LocalInvoker.java
+++ src/main/java/ars/invoke/local/LocalInvoker.java
@@ -15,26 +15,12 @@ import ars.invoke.request.Requester;
  * 
  */
 public class LocalInvoker implements Invoker {
-	/**
-	 * 获取请求资源参数
-	 * 
-	 * @param requester
-	 *            请求对象
-	 * @param function
-	 *            本地资源对象
-	 * @return 参数数组
-	 * @throws Exception
-	 *             操作异常
-	 */
-	protected Object[] getParameters(Requester requester, Function function) throws Exception {
-		return Apis.getParameters(requester, function.getConditions());
-	}
 
 	@Override
 	public Object execute(Requester requester, Resource resource) throws Exception {
 		Function function = (Function) resource;
 		try {
-			return function.getMethod().invoke(function.getTarget(), this.getParameters(requester, function));
+			return function.getMethod().invoke(function.getTarget(), Apis.getParameters(requester, function));
 		} catch (InvocationTargetException e) {
 			Throwable error = e.getTargetException();
 			if (error instanceof Exception) {

540fc8b98723f6cd95700a90cec409f8bfa4c571
wuyq <314691309@qq.com>
2018-03-14 16:58:25 +0800

    Add console property to print log in console

--- src/main/java/ars/invoke/InvokeLogger.java
+++ src/main/java/ars/invoke/InvokeLogger.java
@@ -15,9 +15,10 @@ import ars.invoke.event.InvokeCompleteEvent;
  * 
  */
 public class InvokeLogger implements InvokeListener<InvokeCompleteEvent> {
-	public static final Logger logger = LoggerFactory.getLogger(InvokeLogger.class);
+	protected static final Logger logger = LoggerFactory.getLogger(InvokeLogger.class);
 
 	private String pattern; // 资源地址匹配模式
+	private boolean console; // 是否在控制台打印日志
 
 	public String getPattern() {
 		return pattern;
@@ -27,13 +28,26 @@ public class InvokeLogger implements InvokeListener<InvokeCompleteEvent> {
 		this.pattern = pattern;
 	}
 
+	public boolean isConsole() {
+		return console;
+	}
+
+	public void setConsole(boolean console) {
+		this.console = console;
+	}
+
 	@Override
 	public void onInvokeEvent(InvokeCompleteEvent event) {
 		Object value = event.getValue();
-		if (value instanceof Throwable || (logger.isDebugEnabled()
+		if (value instanceof Throwable || ((this.console || logger.isDebugEnabled())
 				&& (this.pattern == null || Strings.matches(event.getSource().getUri(), this.pattern)))) {
 			String message = Invokes.getLog(event.getSource(), value, event.getTimestamp());
-			if (value instanceof Throwable) {
+			if (this.console) {
+				System.out.print(message);
+				if (value instanceof Throwable) {
+					((Throwable) value).printStackTrace();
+				}
+			} else if (value instanceof Throwable) {
 				logger.error(message, (Throwable) value);
 			} else {
 				logger.debug(message);

3290b9a0512026bb0ae3ae053bdaa37472ff4685
arsframework <postmaster@arsframework.com>
2018-03-26 17:44:53 +0800

    Add the argument of depth

--- src/main/java/ars/invoke/convert/JsonConverter.java
+++ src/main/java/ars/invoke/convert/JsonConverter.java
@@ -7,13 +7,22 @@ import ars.invoke.convert.Converter;
  * 基于json格式的对象数据转换实现
  * 
  * @author yongqiangwu
- *
+ * 
  */
 public class JsonConverter implements Converter {
+	protected final int depth; // json转换对象属性下钻深度
+
+	public JsonConverter() {
+		this(2);
+	}
+
+	public JsonConverter(int depth) {
+		this.depth = depth;
+	}
 
 	@Override
 	public String serialize(Object object) {
-		return Jsons.format(object);
+		return Jsons.format(object, this.depth);
 	}
 
 	@Override

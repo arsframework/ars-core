10249ce98c7418b36a1c562d22b7d2b95b5d78d7
arsframework <postmaster@arsframework.com>
2018-03-26 17:42:48 +0800

    Add the argument of depth

--- src/main/java/ars/util/Jsons.java
+++ src/main/java/ars/util/Jsons.java
@@ -27,42 +27,9 @@ import ars.util.ObjectAdapter;
  * 
  */
 public final class Jsons {
+	private static Gson defaultGson;
 	private static ObjectAdapter[] objectAdapters;
-
-	/**
-	 * Gson通用类型适配器
-	 */
-	private static final TypeAdapter<Object> commonTypeAdapter = new TypeAdapter<Object>() {
-
-		@Override
-		public Object read(JsonReader reader) throws IOException {
-			return null;
-		}
-
-		@Override
-		public void write(JsonWriter writer, Object object) throws IOException {
-			Jsons.write(writer, object, 1);
-		}
-
-	};
-
-	/**
-	 * 完整的JSON解析处理对象
-	 */
-	private static final Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-
-	/**
-	 * 最小化JSON解析处理对象
-	 */
-	private static final Gson minGson = new GsonBuilder().registerTypeAdapterFactory(new TypeAdapterFactory() {
-
-		@SuppressWarnings("unchecked")
-		@Override
-		public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> token) {
-			return (TypeAdapter<T>) commonTypeAdapter;
-		}
-
-	}).disableHtmlEscaping().create();
+	private static final GsonBuilder gsonBuilder = new GsonBuilder();
 
 	private Jsons() {
 
@@ -94,6 +61,204 @@ public final class Jsons {
 	}
 
 	/**
+	 * Gson通用类型适配器
+	 * 
+	 * @author yongqiangwu
+	 * 
+	 */
+	private static class CommonTypeAdapter extends TypeAdapter<Object> {
+		protected final int depth; // 对象属性下钻深度（小于1表示不限制深度）
+
+		public CommonTypeAdapter(int depth) {
+			this.depth = depth;
+		}
+
+		/**
+		 * 将对象写入json
+		 * 
+		 * @param writer
+		 *            json写操作对象
+		 * @param object
+		 *            对象实例
+		 * @param level
+		 *            当前层级
+		 * @throws IOException
+		 *             IO操作异常
+		 */
+		protected void write(JsonWriter writer, Object object, int level) throws IOException {
+			Object adapted;
+			if (object == null || (this.depth > 0 && level > this.depth)) {
+				writer.nullValue();
+			} else if (object instanceof CharSequence) {
+				writer.value(((CharSequence) object).toString());
+			} else if (object instanceof Number) {
+				writer.value((Number) object);
+			} else if (object instanceof Boolean) {
+				writer.value((Boolean) object);
+			} else if (Beans.isMetaClass(object.getClass())) {
+				writer.value(Strings.toString(object));
+			} else if ((adapted = adaption(object)) != object) {
+				write(writer, adapted, level);
+			} else if (object instanceof Formable) {
+				write(writer, ((Formable) object).format(), level + 1);
+			} else if (object instanceof Map) {
+				writer.beginObject();
+				for (Entry<?, ?> entry : ((Map<?, ?>) object).entrySet()) {
+					writer.name(Strings.toString(entry.getKey()));
+					write(writer, entry.getValue(), 1);
+				}
+				writer.endObject();
+			} else if (object instanceof Collection) {
+				writer.beginArray();
+				for (Object value : ((Collection<?>) object)) {
+					write(writer, value, 1);
+				}
+				writer.endArray();
+			} else if (object instanceof Object[]) {
+				writer.beginArray();
+				for (Object value : ((Object[]) object)) {
+					write(writer, value, 1);
+				}
+				writer.endArray();
+			} else if (object instanceof int[]) {
+				writer.beginArray();
+				for (int value : ((int[]) object)) {
+					writer.value(value);
+				}
+				writer.endArray();
+			} else if (object instanceof long[]) {
+				writer.beginArray();
+				for (long value : ((long[]) object)) {
+					writer.value(value);
+				}
+				writer.endArray();
+			} else if (object instanceof short[]) {
+				writer.beginArray();
+				for (short value : ((short[]) object)) {
+					writer.value(value);
+				}
+				writer.endArray();
+			} else if (object instanceof float[]) {
+				writer.beginArray();
+				for (float value : ((float[]) object)) {
+					writer.value(value);
+				}
+				writer.endArray();
+			} else if (object instanceof double[]) {
+				writer.beginArray();
+				for (double value : ((double[]) object)) {
+					writer.value(value);
+				}
+				writer.endArray();
+			} else if (object instanceof char[]) {
+				writer.beginArray();
+				for (char value : ((char[]) object)) {
+					writer.value(String.valueOf(value));
+				}
+				writer.endArray();
+			} else if (object instanceof byte[]) {
+				writer.beginArray();
+				for (byte value : ((byte[]) object)) {
+					writer.value(value);
+				}
+				writer.endArray();
+			} else if (object instanceof boolean[]) {
+				writer.beginArray();
+				for (boolean value : ((boolean[]) object)) {
+					writer.value(value);
+				}
+				writer.endArray();
+			} else {
+				writer.beginObject();
+				Class<?> type = object.getClass();
+				while (type != Object.class) {
+					for (Field field : type.getDeclaredFields()) {
+						if (Modifier.isStatic(field.getModifiers())) {
+							continue;
+						}
+						Object value;
+						field.setAccessible(true);
+						try {
+							value = field.get(object);
+						} catch (IllegalAccessException e) {
+							throw new RuntimeException(e);
+						} finally {
+							field.setAccessible(false);
+						}
+						writer.name(field.getName());
+						if (value instanceof CharSequence) {
+							value = Strings.escape((CharSequence) value);
+						}
+						if (value == null || value instanceof CharSequence || value instanceof Number
+								|| value instanceof Boolean || Beans.isMetaClass(value.getClass())) {
+							write(writer, value, level);
+						} else {
+							write(writer, value, level + 1);
+						}
+					}
+					type = type.getSuperclass();
+				}
+				writer.endObject();
+			}
+		}
+
+		@Override
+		public Object read(JsonReader reader) throws IOException {
+			return null;
+		}
+
+		@Override
+		public void write(JsonWriter writer, Object object) throws IOException {
+			this.write(writer, object, 1);
+		}
+
+	}
+
+	/**
+	 * 构建json处理对象
+	 * 
+	 * @return json处理对象
+	 */
+	public static Gson buildGson() {
+		return buildGson(-1);
+	}
+
+	/**
+	 * 构建json处理对象
+	 * 
+	 * @param depth
+	 *            对象属性下钻深度
+	 * @return json处理对象
+	 */
+	public static Gson buildGson(final int depth) {
+		return gsonBuilder.registerTypeAdapterFactory(new TypeAdapterFactory() {
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> token) {
+				return (TypeAdapter<T>) new CommonTypeAdapter(depth);
+			}
+
+		}).disableHtmlEscaping().create();
+	}
+
+	/**
+	 * 获取默认json处理对象
+	 * 
+	 * @return json处理对象
+	 */
+	public static Gson getDefaultGson() {
+		if (defaultGson == null) {
+			synchronized (Jsons.class) {
+				if (defaultGson == null) {
+					defaultGson = gsonBuilder.create();
+				}
+			}
+		}
+		return defaultGson;
+	}
+
+	/**
 	 * 对象适配
 	 * 
 	 * @param object
@@ -110,151 +275,18 @@ public final class Jsons {
 	}
 
 	/**
-	 * 将对象写入json
-	 * 
-	 * @param writer
-	 *            json写操作对象
-	 * @param object
-	 *            对象实例
-	 * @param level
-	 *            当前层级
-	 * @throws IOException
-	 *             IO操作异常
-	 */
-	private static void write(JsonWriter writer, Object object, int level) throws IOException {
-		Object adapted;
-		if (object == null || level > 2) {
-			writer.nullValue();
-		} else if (object instanceof CharSequence) {
-			writer.value(((CharSequence) object).toString());
-		} else if (object instanceof Number) {
-			writer.value((Number) object);
-		} else if (object instanceof Boolean) {
-			writer.value((Boolean) object);
-		} else if (Beans.isMetaClass(object.getClass())) {
-			writer.value(Strings.toString(object));
-		} else if ((adapted = adaption(object)) != object) {
-			write(writer, adapted, level);
-		} else if (object instanceof Formable) {
-			write(writer, ((Formable) object).format(), level + 1);
-		} else if (object instanceof Map) {
-			writer.beginObject();
-			for (Entry<?, ?> entry : ((Map<?, ?>) object).entrySet()) {
-				writer.name(Strings.toString(entry.getKey()));
-				write(writer, entry.getValue(), 1);
-			}
-			writer.endObject();
-		} else if (object instanceof Collection) {
-			writer.beginArray();
-			for (Object value : ((Collection<?>) object)) {
-				write(writer, value, 1);
-			}
-			writer.endArray();
-		} else if (object instanceof Object[]) {
-			writer.beginArray();
-			for (Object value : ((Object[]) object)) {
-				write(writer, value, 1);
-			}
-			writer.endArray();
-		} else if (object instanceof int[]) {
-			writer.beginArray();
-			for (int value : ((int[]) object)) {
-				writer.value(value);
-			}
-			writer.endArray();
-		} else if (object instanceof long[]) {
-			writer.beginArray();
-			for (long value : ((long[]) object)) {
-				writer.value(value);
-			}
-			writer.endArray();
-		} else if (object instanceof short[]) {
-			writer.beginArray();
-			for (short value : ((short[]) object)) {
-				writer.value(value);
-			}
-			writer.endArray();
-		} else if (object instanceof float[]) {
-			writer.beginArray();
-			for (float value : ((float[]) object)) {
-				writer.value(value);
-			}
-			writer.endArray();
-		} else if (object instanceof double[]) {
-			writer.beginArray();
-			for (double value : ((double[]) object)) {
-				writer.value(value);
-			}
-			writer.endArray();
-		} else if (object instanceof char[]) {
-			writer.beginArray();
-			for (char value : ((char[]) object)) {
-				writer.value(String.valueOf(value));
-			}
-			writer.endArray();
-		} else if (object instanceof byte[]) {
-			writer.beginArray();
-			for (byte value : ((byte[]) object)) {
-				writer.value(value);
-			}
-			writer.endArray();
-		} else if (object instanceof boolean[]) {
-			writer.beginArray();
-			for (boolean value : ((boolean[]) object)) {
-				writer.value(value);
-			}
-			writer.endArray();
-		} else {
-			writer.beginObject();
-			Class<?> type = object.getClass();
-			while (type != Object.class) {
-				for (Field field : type.getDeclaredFields()) {
-					if (Modifier.isStatic(field.getModifiers())) {
-						continue;
-					}
-					Object value;
-					field.setAccessible(true);
-					try {
-						value = field.get(object);
-					} catch (IllegalAccessException e) {
-						throw new RuntimeException(e);
-					} finally {
-						field.setAccessible(false);
-					}
-					writer.name(field.getName());
-					if (value instanceof CharSequence) {
-						value = Strings.escape((CharSequence) value);
-					}
-					if (value == null || value instanceof CharSequence || value instanceof Number
-							|| value instanceof Boolean || Beans.isMetaClass(value.getClass())) {
-						write(writer, value, level);
-					} else {
-						write(writer, value, level + 1);
-					}
-				}
-				type = type.getSuperclass();
-			}
-			writer.endObject();
-		}
-	}
-
-	/**
 	 * 将对象转换成JSON字符串
-	 * 
-	 * 对象向下关联一级
 	 * 
 	 * @param object
 	 *            被转换对象
 	 * @return JSON字符串
 	 */
 	public static String format(Object object) {
-		return format(object, true);
+		return format(object, -1);
 	}
 
 	/**
 	 * 将对象转换成JSON字符串
-	 * 
-	 * 对象向下关联一级
 	 * 
 	 * @param object
 	 *            被转换对象
@@ -263,9 +295,23 @@ public final class Jsons {
 	 * @return JSON字符串
 	 */
 	public static String format(Object object, boolean min) {
-		return object == null ? null
-				: object instanceof CharSequence ? ((CharSequence) object).toString()
-						: min ? minGson.toJson(object) : gson.toJson(object);
+		return format(object, min ? 2 : -1);
+	}
+
+	/**
+	 * 将对象转换成JSON字符串
+	 * 
+	 * 对象向下关联一级
+	 * 
+	 * @param object
+	 *            被转换对象
+	 * @param depth
+	 *            对象属性关联深度
+	 * @return JSON字符串
+	 */
+	public static String format(Object object, int depth) {
+		return object == null ? null : object instanceof CharSequence ? ((CharSequence) object).toString() : buildGson(
+				depth).toJson(object);
 	}
 
 	/**
@@ -291,7 +337,7 @@ public final class Jsons {
 	 * @return 对象实例
 	 */
 	public static <T> T parse(Class<T> type, String json) {
-		return Strings.isEmpty(json) ? null : gson.fromJson(json, type);
+		return Strings.isEmpty(json) ? null : getDefaultGson().fromJson(json, type);
 	}
 
 }
